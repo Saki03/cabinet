d5c95f5cc845c0d7842a2f815cd105c9214df362
saki kamata <saki@MacBook-Air-2.local>
2018-04-12 17:02:02 +0900

    Edit and Destory Docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -26,9 +26,16 @@ class DocsController < ApplicationController
   end
 
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @doc.destory
+    redirect_to docs_path
   end
 
   private
